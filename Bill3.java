8a6e1b0bf56f676eef6a0168be9ab1f54ac7ee38
unknown <matanyacutler@icloud.com>
2025-10-29 18:33:00 +0200

    corrected ex 1

--- Bill3.java
+++ Bill3.java
@@ -8,6 +8,8 @@ public class Bill3 {
 		int sum = Integer.parseInt(args[3]);
 		double sum1 = Math.ceil(sum / 3.0);
 		System.out.println("Dear " + name3 + ", " + name2 +
-		" and " + name1 + ": pay " + sum1 + " each");   
+		" and " + name1 + ": pay " + sum1 + " Shekels each");   
 	}
 }
+
+
